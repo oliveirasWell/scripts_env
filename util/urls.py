90d34c1be8100b5042cd7afda1f600acc5e765b3
Wellington de Oliveira dos Santos <well.oliveira.snt@gmail.com>
2017-08-07 06:31:23 -0400

    Scripts iniciais

--- util/urls.py
+++ util/urls.py
@@ -1,43 +1,5 @@
 repos = [
-    'https://bitbucket.org/tecsinapse/tecsinapse-helpdesk',
-    'https://bitbucket.org/tecsinapse/hdb-portal',
-    'https://bitbucket.org/tecsinapse/proto-portal',
-    'https://bitbucket.org/tecsinapse/jenkins-job-generator',
-    'https://bitbucket.org/tecsinapse/cavadinha',
-
-    'https://bitbucket.org/tecsinapse/tecsinapse-keycloak',
-    'https://bitbucket.org/tecsinapse/bonsai',
-    'https://bitbucket.org/tecsinapse/land-fluxo',
-    'https://bitbucket.org/tecsinapse/sin-plus',
-    'https://bitbucket.org/tecsinapse/audi',
-    'https://bitbucket.org/tecsinapse/gestaorede',
-    'https://bitbucket.org/tecsinapse/gestaodealer-produto',
-
-    'https://bitbucket.org/tecsinapse/code-revision',
-    'https://bitbucket.org/tecsinapse/meteor-tp',
-    'https://bitbucket.org/tecsinapse/tecsinapse-keycloak-server',
-
-    'https://gitlab.com/indext/php-site',
-    'https://gitlab.com/indext/django-raspberry',
-    'https://gitlab.com/indext/bep-web',
-    'https://gitlab.com/indext/bep-mobile',
-    'https://gitlab.com/indext/bep',
-    'https://gitlab.com/indext/pec-br',
-    'https://gitlab.com/indext/ministerio-cidades',
-
-    'https://github.com/oliveirasWell/firebase-esp8266-dht11-logger',
-    'https://github.com/oliveirasWell/lastfmparser',
-    'https://github.com/oliveirasWell/arduino-web-home-control',
-    'https://github.com/oliveirasWell/tudo-gostosissimo',
-    'https://github.com/oliveirasWell/processor-mvl',
-    'https://github.com/oliveirasWell/mvl-compiler'
-
 ]
 
 gists = [
-    'https://gist.github.com/59598fab62bbf80c52f7504018d301d0.git',
-    'https://gist.github.com/efd8d8e57ee5cc4d1478ccc7e7517aa5.git',
-    'https://gist.github.com/45235b69b49f9faaef6959728d62320f.git',
-    'https://gist.github.com/c55ece318ce8fc19f4660cc5e1e01c0e.git',
-    'https://gist.github.com/2eebd2447a10708767ba59bf390f7bbf.git'
 ]
